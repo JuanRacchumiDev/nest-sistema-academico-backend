ee289d1c325b15589c4c773d0d23a280188a2806
JuanRacchumi <juan.racchumi.dev@gmail.com>
2026-09-24 23:32:30 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService)
   const port = configService.get<number>('PORT', 3004)
+
+  // Configura los límites del parser del body
+  app.use(json({ limit: '20mb' }));
+  app.use(urlencoded({ extended: true, limit: '20mb' }));
 
   // Pipe global para transformar y validar los datos recibidos en controladores
   app.useGlobalPipes(
